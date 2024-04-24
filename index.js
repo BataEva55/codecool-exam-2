040b75b828afeeb23db2d6a178c4592f8ddc10e6
Your Name <you@example.com>
2024-04-24 12:20:27 +0200

    task solving

--- index.js
+++ index.js
@@ -6,6 +6,29 @@
 // odd: an array of all odd numbers from arr.
 // If the input is not an array, the function should throw an error with message: "Input must be an array"
 export function groupArrayByParity(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("Input must be an array");
+  }
+  if (!arr.every((item) => typeof item === "number")) {
+    throw new Error("All elements must be numbers");
+  }
+
+  let odd = [];
+  let even = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      even.push(arr[i]);
+    } else {
+      odd.push(arr[i]);
+    }
+  }
+  const returnObject = {
+    odd,
+    even,
+  };
+
+  return returnObject;
 }
 
 //Task: averageWordCount
@@ -13,6 +36,31 @@ export function groupArrayByParity(arr) {
 //The function should return the average number of words (with a length greater than minLength) for each sentence.
 //If sentences is empty, the function should return 0.
 export function averageWordCount(sentences, minLength) {
+  if (!Array.isArray(sentences)) {
+    throw new Error("Input sentences must be an array");
+  }
+
+  if (sentences.length === 0) {
+    return 0;
+  }
+
+  let totalWords = 0;
+  let validSentencesCount = 0;
+
+  sentences.forEach((sentence) => {
+    const words = sentence.split(" ");
+    const validWordsCount = words.filter((word) => word.length > minLength).length;
+    if (validWordsCount > 0) {
+      totalWords += validWordsCount;
+      validSentencesCount++;
+    }
+  });
+  const average = totalWords / validSentencesCount;
+  if (isNaN(average)) {
+    return 0;
+  } else {
+    return average;
+  }
 }
 
 // Task: findHighestScore
@@ -21,15 +69,43 @@ export function averageWordCount(sentences, minLength) {
 // Each object in the students array contains the name of a student
 // and their scores in different subjects. The function should
 // return the name of the student with the highest score in the specified subject.
-// 
+//
 // If no student has a score for the specified subject,
 // or if students is empty, the function should return null.
 // If the students array contains elements that aren't objects
 // or if subject isn't a string, the function should throw an error.
-// 
+//
 // Examples:
 // findHighestScore([ { name: 'Alice', math: 90, science: 85 }, { name: 'Bob', math: 95, science: 80 } ], 'math') should return 'Bob'.
 // findHighestScore([ { name: 'Alice', math: 90 }, { name: 'Bob', science: 95 } ], 'math') should return 'Alice'.
 // findHighestScore([ { name: 'Alice', math: 90 }, { name: 'Bob', science: 95 } ], 'history') should return null.
 export function findHighestScore(students, subject) {
+  if (!Array.isArray(students)) {
+    throw new Error("Input students must be an array");
+  }
+
+  if (typeof subject !== "string") {
+    throw new Error("Input subject must be a string");
+  }
+  if (students.length === 0) {
+    return null;
+  }
+
+  let highestScore = -Infinity;
+  let studentWithHighestScore = null;
+
+  students.forEach((student) => {
+    if (typeof student === "object" && student !== null) {
+      if (subject in student) {
+        if (student[subject] > highestScore) {
+          highestScore = student[subject];
+          studentWithHighestScore = student.name;
+        }
+      }
+    } else {
+      throw new Error("Invalid student object found in students array");
+    }
+  });
+
+  return studentWithHighestScore;
 }
--- static/index.js
+++ static/index.js
@@ -1,7 +1,46 @@
-import data from './data.js';
+import data from "./data.js";
+
+const root = document.querySelector("#root");
+
+const section = document.createElement("section");
+section.id = "clean-code-principles";
+
+const h1 = document.createElement("h1");
+h1.className = "code__h1";
+h1.innerText = "Clean Code Principles";
+
+section.append(h1);
+root.append(section);
 
 function main() {
-  console.log(data);
+  data.forEach((item) => {
+    const flatListContainer = document.createElement("div");
+    flatListContainer.className = "flat-list";
+
+    const titleBlock = document.createElement("div");
+    titleBlock.className = "flat-list__title";
+
+    flatListContainer.appendChild(titleBlock);
+
+    const card = document.createElement("div");
+    card.className = "card";
+
+    const cardTitle = document.createElement("div");
+    cardTitle.className = "card__title";
+    cardTitle.innerText = item.title;
+
+    const cardBody = document.createElement("div");
+    cardBody.className = "card__body";
+    cardBody.innerText = item.description;
+
+    card.appendChild(cardTitle);
+    card.appendChild(cardBody);
+
+    flatListContainer.appendChild(card);
+    section.appendChild(flatListContainer);
+
+    root.appendChild(section);
+  });
 }
 
 main();
